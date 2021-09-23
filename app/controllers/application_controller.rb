e30a09f42d206406f4c47bd7d7a4ba4d362bda5b
mizuki <mizukisb@gmail.com>
2021-09-23 14:24:55 +1000

    add service card frame

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper_method :available_currencies
+
+  def available_currencies
+    @available_currencies ||= begin
+      res = ExchangerateHost.supported_symbols
+      res['symbols'].keys
+    end
+  end
 end
